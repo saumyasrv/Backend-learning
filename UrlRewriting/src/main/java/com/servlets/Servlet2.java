fd48b026640b6af4b2fe2836842f5ef5312eeedf
saumyasrv <saumyasrv10@gmail.com>
2025-06-19 01:22:22 +0530

    Add session tracking technique 2

---
+++ UrlRewriting/src/main/java/com/servlets/Servlet2.java
@@ -0,0 +1,31 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Servlet2")
+public class Servlet2 extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 PrintWriter out = response.getWriter() ;
+		 out.println("<h1 style='color:blue;'>Welcome to servlet 2</h1>");
+		 
+		 String name = request.getParameter("user");
+		 out.println("<h1 style='color:red;'>Welcome back "+name+"</h1>");
+
+		 
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
